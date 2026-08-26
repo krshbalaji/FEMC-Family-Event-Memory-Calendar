be1ca589ceb1265bfc435b4553eae2688a061fcb
Balaji A K <balajia.kbalaji@gmail.com>
2026-08-26 13:27:50 +0530

    fix: install event celebration integration patch

--- run.py
+++ run.py
@@ -2,10 +2,12 @@ import runtime
 from launch_projection_patch import install
 from event_edit_patch import install as install_event_edit
 from event_create_sync_patch import install as install_event_create_sync
+from event_celebration_integration_patch import install as install_event_celebration_integration
 
 install(runtime)
 install_event_edit(runtime)
 install_event_create_sync(runtime)
+install_event_celebration_integration(runtime)
 
 
 if __name__ == "__main__":
