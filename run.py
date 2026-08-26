cc63218955917914c1a540e65239a5498de7c04a
Balaji A K <balajia.kbalaji@gmail.com>
2026-08-26 15:39:56 +0530

    feat: install Mayil event intelligence patch

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ from event_celebration_integration_patch import install as install_event_celebra
 from sharing_label_patch import install as install_sharing_label_patch
 from practice_sharing_celebration_projection_patch import install as install_practice_sharing_celebration_projection
 from practice_shared_celebration_context_patch import install as install_practice_shared_celebration_context
+from event_intelligence_patch import install as install_event_intelligence
 
 install(runtime)
 install_event_edit(runtime)
@@ -14,6 +15,7 @@ install_event_celebration_integration(runtime)
 install_sharing_label_patch(runtime)
 install_practice_sharing_celebration_projection(runtime)
 install_practice_shared_celebration_context(runtime)
+install_event_intelligence(runtime)
 
 
 if __name__ == "__main__":
